6805d9ac9b65a0bc24a544e21021bed26d039890
Jeremy <acnajcadcac.yahoo.com>
2017-01-11 16:04:03 -0600

    Added a counter to keep track of how may guesses

--- Guess_The_Number.py
+++ Guess_The_Number.py
@@ -8,16 +8,17 @@ print("Guess a number between 1-100")
 import random
 randomNumber = random.randrange(1,100) # settig the random number to be between 1-100
 userGuess = int(input("Guess here: ")) # getting the user input
-
+count = 1
 while(userGuess != randomNumber): # while loop to keep going until the right number is guessed
 
     if userGuess > randomNumber: # letting the user know the number is too high
         print(userGuess, " is too high, guess again")
-
+        count += 1
         userGuess = int(input("Guess here: "))
 
     elif userGuess < randomNumber: # letting the user know the number is too low
         print(userGuess, " is too low, guess again")
+        count += 1
         userGuess = int(input("Guess here: "))
     
-print("You guessed the right number! The number was", randomNumber) # will print when the user get the number right
+print("You guessed the right number! It took you", count, "guesses. The number was", randomNumber) # will print when the user get the number right
